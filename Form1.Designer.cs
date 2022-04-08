d048d2d94960bc7f105ab717b2919ebf74ad756d
freedomisatrap <88254418+freedomisatrap@users.noreply.github.com>
2022-04-08 21:01:52 +0200

    some clearing

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace carshop
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+
+ 
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.buttonEmployees = new System.Windows.Forms.Button();
+            this.buttonDepartments = new System.Windows.Forms.Button();
+            this.buttonCustomers = new System.Windows.Forms.Button();
+            this.buttonCar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelTitle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelMenu.Controls.Add(this.buttonEmployees);
+            this.panelMenu.Controls.Add(this.buttonDepartments);
+            this.panelMenu.Controls.Add(this.buttonCustomers);
+            this.panelMenu.Controls.Add(this.buttonCar);
+            this.panelMenu.Controls.Add(this.panel1);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(265, 676);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // buttonEmployees
+            // 
+            this.buttonEmployees.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonEmployees.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEmployees.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonEmployees.Location = new System.Drawing.Point(0, 447);
+            this.buttonEmployees.Name = "buttonEmployees";
+            this.buttonEmployees.Size = new System.Drawing.Size(265, 123);
+            this.buttonEmployees.TabIndex = 1;
+            this.buttonEmployees.Text = "Employees";
+            this.buttonEmployees.UseVisualStyleBackColor = false;
+            this.buttonEmployees.Click += new System.EventHandler(this.buttonEmployees_Click);
+            // 
+            // buttonDepartments
+            // 
+            this.buttonDepartments.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonDepartments.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDepartments.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonDepartments.Location = new System.Drawing.Point(0, 331);
+            this.buttonDepartments.Name = "buttonDepartments";
+            this.buttonDepartments.Size = new System.Drawing.Size(265, 123);
+            this.buttonDepartments.TabIndex = 1;
+            this.buttonDepartments.Text = "Departments";
+            this.buttonDepartments.UseVisualStyleBackColor = false;
+            this.buttonDepartments.Click += new System.EventHandler(this.buttonDepartments_Click);
+            // 
+            // buttonCustomers
+            // 
+            this.buttonCustomers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonCustomers.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCustomers.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonCustomers.Location = new System.Drawing.Point(0, 212);
+            this.buttonCustomers.Name = "buttonCustomers";
+            this.buttonCustomers.Size = new System.Drawing.Size(265, 123);
+            this.buttonCustomers.TabIndex = 1;
+            this.buttonCustomers.Text = "Customers";
+            this.buttonCustomers.UseVisualStyleBackColor = false;
+            this.buttonCustomers.Click += new System.EventHandler(this.buttonCustomers_Click);
+            // 
+            // buttonCar
+            // 
+            this.buttonCar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonCar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonCar.Location = new System.Drawing.Point(0, 92);
+            this.buttonCar.Name = "buttonCar";
+            this.buttonCar.Size = new System.Drawing.Size(265, 123);
+            this.buttonCar.TabIndex = 0;
+            this.buttonCar.Text = "Car";
+            this.buttonCar.UseVisualStyleBackColor = false;
+            this.buttonCar.Click += new System.EventHandler(this.buttonCar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(259, 94);
+            this.panel1.TabIndex = 0;
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panelTitle.Controls.Add(this.label1);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(265, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(1064, 100);
+            this.panelTitle.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Good Times", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(487, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Home";
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(301, 115);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(911, 509);
+            this.panel2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1329, 676);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelTitle.ResumeLayout(false);
+            this.panelTitle.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button buttonCar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonEmployees;
+        private System.Windows.Forms.Button buttonDepartments;
+        private System.Windows.Forms.Button buttonCustomers;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
